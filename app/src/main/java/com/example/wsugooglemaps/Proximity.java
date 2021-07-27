0b39f3b34687120f4a4c1fce4059acd95e7aad86
Ali Belkasri <agbelkasri@gmail.com>
2021-07-27 17:43:37 -0400

    Update Proximity.java

--- app/src/main/java/com/example/wsugooglemaps/Proximity.java
+++ app/src/main/java/com/example/wsugooglemaps/Proximity.java
@@ -1,4 +1,26 @@
 package com.example.wsugooglemaps;
 
 public class Proximity {
+    SensorEventListener proximitySensorEventListener = new SensorEventListener() {
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // method to check accuracy changed in sensor.
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            // check if the sensor type is proximity sensor.
+            if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
+                if (event.values[0] == 0) {
+                    // here we are setting our status to our textview..
+                    // if sensor event return 0 then object is closed
+                    // to sensor else object is away from sensor.
+                    sensorStatusTV.setText("Near");
+                } else {
+                    sensorStatusTV.setText("Away");
+                }
+            }
+        }
+    };
+}
 }
